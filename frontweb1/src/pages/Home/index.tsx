fb8dd5070025b6893a6e674afc0202198ec45143
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-03-08 17:41:54 -0300

    Os commitados

--- frontweb1/src/pages/Home/index.tsx
+++ frontweb1/src/pages/Home/index.tsx
@@ -15,9 +15,11 @@ const Home = () => {
               mercado
             </p>
           </div>
-          <Link to="/products">
-            <ButtonIcon />
-          </Link>
+          <div>
+            <Link to="/products">
+              <ButtonIcon />
+            </Link>
+          </div>
         </div>
         <div className="home-image-container">
           <MainImage />
