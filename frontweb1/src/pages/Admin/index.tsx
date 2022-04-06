d1d9506bfb919e2135a191f24f81437f075aa115
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-04-05 21:40:56 -0300

    Admin Navbar layout mobile

--- frontweb1/src/pages/Admin/index.tsx
+++ frontweb1/src/pages/Admin/index.tsx
@@ -1,4 +1,15 @@
+import Navbar from './Navbar';
+import './styles.css'
+
 const Admin = () => {
-  return <h1>Pagina Admin</h1>;  
+  return (
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+        <h1>Conteudo</h1>
+      </div>
+    </div>
+  );
 };
+
 export default Admin;
