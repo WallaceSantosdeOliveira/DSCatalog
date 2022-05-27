52ebba8692cf69732b84047880bbb2c1cbb3c857
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-05-26 22:57:45 -0300

    Banner styling

--- frontweb1/src/pages/Admin/Auth/index.tsx
+++ frontweb1/src/pages/Admin/Auth/index.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
-  
+import './styles.css';  
 const Auth = () => {
   
   return (
