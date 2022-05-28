fc5aa9c4a23d32790ce0bcf16bc64a40e34f3499
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-05-28 11:08:30 -0300

    Manual error handling

--- frontweb1/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb1/src/pages/Admin/Auth/Login/index.tsx
@@ -2,23 +2,28 @@ import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { requestBackendLogin } from 'util/requests';
-import './styles.css';
+import { useState } from 'react';
 
+import './styles.css';
 
 type FormData = {
   username: string;
   password: string;
-}
+};
 
 const Login = () => {
+  const [hasError, setHasError] = useState(false);
+
   const { register, handleSubmit } = useForm<FormData>();
 
-  const onSubmit = (formData : FormData) => {
-    requestBackendLogin(formData) 
-    .then(response => {
-      console.log('SUCESSO', response);
-    })
-      .catch(error=> {
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        setHasError(false);
+        console.log('SUCESSO', response);
+      })
+      .catch((error) => {
+        setHasError(true);
         console.log('ERRO', error);
       });
   };
@@ -26,6 +31,11 @@ const Login = () => {
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">
+          Erro ao tentar efetuar o Login
+        </div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
@@ -60,5 +70,5 @@ const Login = () => {
       </form>
     </div>
   );
-}
+};
 export default Login;
