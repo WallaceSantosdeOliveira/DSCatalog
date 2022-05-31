51c0c7647cda87f38290bc178f3e86f98e88fc5f
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-05-31 13:02:32 -0300

    Redirecting programmatically

--- frontweb1/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb1/src/pages/Admin/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { getAuthData, requestBackendLogin, saveAuthData } from 'util/requests';
@@ -16,15 +16,17 @@ const Login = () => {
 
   const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
+  const history = useHistory();
+
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
         saveAuthData(response.data);
         const token = getAuthData().access_token;
-        console.log('TOKEN GERADO: ' + token);
-        
+        console.log('TOKEN GERADO: ' + token);        
         setHasError(false);
         console.log('SUCESSO', response);
+        history.push('/admin');
       })
       .catch((error) => {
         setHasError(true);
