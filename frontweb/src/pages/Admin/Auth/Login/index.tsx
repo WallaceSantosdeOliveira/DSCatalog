46d066d494eda55d08ef4d7d444494adb6f7db04
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-06-07 14:49:06 -0300

    Login request

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
+import { requestBackendLogin } from 'util/requests';
 
 import './styles.css';
+
 
 type FormData = {
     username: string;
@@ -14,7 +16,13 @@ const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (formData : FormData) => {
-      console.log(formData);
+      requestBackendLogin(formData)
+      .then(response => {
+          console.log('SUCESSO', response);
+      })
+      .catch(error => {
+          console.log('ERRO', error);
+      });
   };
 
   return (
