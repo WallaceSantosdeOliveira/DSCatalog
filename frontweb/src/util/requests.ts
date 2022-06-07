9615514d51011b41e88cc67a81fe595846667e93
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-06-07 19:24:18 -0300

    Autorized request

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 
 type LoginResponse = {
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-    scope: string;
-    userFirstName: string;
-    userId: number;
-}
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
 
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
-const tokenkey = 'authData'
+const tokenkey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
@@ -26,22 +26,39 @@ type LoginData = {
 export const requestBackendLogin = (loginData: LoginData) => {
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
+    Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
   };
 
   const data = qs.stringify({
     ...loginData,
-    grant_type: 'password'
+    grant_type: 'password',
   });
 
-  return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
-}
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
+};
 
-export const saveAuthData = (obj : LoginResponse) => {
-    localStorage.setItem(tokenkey, JSON.stringify(obj));
-}
+export const requestBackend = (config: AxiosRequestConfig) => {
+  const headers = config.withCredentials
+    ? {
+        ...config.headers,
+        Authorization: 'Bearer ' + getAuthData().access_token,
+      }
+    : config.headers;
+
+  return axios({ ...config, baseURL: BASE_URL, headers });
+};
+
+export const saveAuthData = (obj: LoginResponse) => {
+  localStorage.setItem(tokenkey, JSON.stringify(obj));
+};
 
 export const getAuthData = () => {
-    const str = localStorage.getItem(tokenkey) ?? "{}";
-    return JSON.parse(str) as LoginResponse;    
-}
+  const str = localStorage.getItem(tokenkey) ?? '{}';
+  return JSON.parse(str) as LoginResponse;
+};
