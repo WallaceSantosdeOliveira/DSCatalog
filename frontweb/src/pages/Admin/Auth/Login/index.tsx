e895ab998cf388dbd764a93d9764af703676d402
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-06-07 16:22:19 -0300

    Conditional styling

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -45,7 +45,7 @@ const Login = () => {
                 }
             })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`}
             placeholder="Email"
             name="username"
           />
@@ -57,7 +57,7 @@ const Login = () => {
                 required: 'Campo obrigatorio'
             })}
             type="password"
-            className="form-control base-input "
+            className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`}
             placeholder="Password"
             name="password"
           />
