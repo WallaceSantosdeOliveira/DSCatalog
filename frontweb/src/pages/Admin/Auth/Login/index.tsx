cc16ee456d58bbceca88dfaa871178cf6710b60c
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-06-07 16:07:45 -0300

    Form validation

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -14,7 +14,7 @@ type FormData = {
 const Login = () => {
   const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
@@ -31,30 +31,37 @@ const Login = () => {
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-
       {hasError && (
-        <div className="alert alert-danger">
-          Erro ao tentar efetuar o login
-        </div>
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
       )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
-            {...register('username')}
+            {...register('username', {
+                required: 'Campo obrigatorio',
+                pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Email invalido'
+                }
+            })}
             type="text"
             className="form-control base-input"
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">{errors.username?.message}</div>
         </div>
         <div className="mb-2">
           <input
-            {...register('password')}
+            {...register('password', {
+                required: 'Campo obrigatorio'
+            })}
             type="password"
             className="form-control base-input "
             placeholder="Password"
             name="password"
           />
+          <div className="invalid-feedback d-block">{errors.password?.message}</div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
