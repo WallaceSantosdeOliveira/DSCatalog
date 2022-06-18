c337c5f6c4a1a8322e02718f6bc0a76e05100004
Wallace SantosdeOliveira <wallacesantos447@gmail.com>
2022-06-18 14:02:39 -0300

    UI restrictions based on user roles

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { hasAnyRoles } from 'util/requests';
 
 import './styles.css';
+
 const Home = () => {
   return (
     <div className="home-container">     
-        <h1>Resultado = {hasAnyRoles(['ROLE_ADMIN']) ? 'Sim' : 'Não'}</h1>
-      <div className="base-card home-card">
+       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
             <h1>Conheça o melhor catálogo de produtos</h1>
